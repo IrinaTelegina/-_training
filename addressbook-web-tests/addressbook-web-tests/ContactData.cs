d4f7b4b35072fb54a1bb6643b5dfa0f1b8160706
Irina.Telegina <Irina.Telegina@KWSNBTTELEGINA>
2021-03-29 20:32:49 +0300

    Создан контакт с Именем и фамилией

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAdressbookTests
+{
+    class ContactData
+    {
+        private string name;
+        private string lastname;
+
+        public ContactData(string name, string lastname)
+        {
+            this.name = name;
+            this.lastname = lastname;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
+
